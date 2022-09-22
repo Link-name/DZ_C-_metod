ac2e7218e0a7c17b1ab1cd9bd4993bc433afda4b
Link <stereo_house01@mail.ru>
2022-09-22 12:05:46 +0300

    работа c методами

---
+++ Work_DZ02/Program.cs
@@ -0,0 +1,51 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        Console.WriteLine(collection[index]);
+        index = index + 1;
+
+    }
+}
+
+void PrintArray(int[] mass)
+
+{
+    int count = mass.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(mass[position]);
+        position++;
+    }
+
+}
+
+int indexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0; 
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+        }
+
+            index++;
+
+    }
+    return position; 
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int rus = indexOf(array, 4);
